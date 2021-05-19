fabb6714a4709e334bf77ddb2a733893d755bb13
N8Brown <nathan.a.brown@outlook.com>
2021-05-19 09:48:46 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# PDF Generator
+
+A simple Python application with a Tkinter graphical user interface that allows the user to generate a PDF from a given file or list of files.
+
+## Technologies
+* Python 3.9.2
---
+++ pdf_generator.py
@@ -0,0 +1,20 @@
+"""
+File Name: pdf_generator.py
+Author: Nathan Brown
+Date Created: 05/19/2021
+Python Version: 3.9.2
+"""
+
+from tkinter import *
+from tkinter import filedialog
+
+root = Tk()
+root.geometry('600x400')
+root.title('PDF Generator')
+
+
+
+
+
+
+root.mainloop()
